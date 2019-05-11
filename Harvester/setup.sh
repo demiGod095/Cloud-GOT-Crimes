6a73d8b830719047a439cb342b39c7d423b78c85
Shreyas Walvekar <shreyas.whis@gmail.com>
2019-05-11 17:13:55 +1000

    Added dummy set up script

---
+++ Harvester/setup.sh
@@ -0,0 +1,15 @@
+echo "Running setup script for python environment"
+
+# sudo apt-get update -y
+# sudo apt-get upgrade -y
+# sudo apt-get dist-upgrade -y
+# sudo apt-get install python3
+
+# sudo yum update -y
+# sudo yum install python3 -y
+
+# python3 -m pip install --upgrade pip
+# pip3 install tweepy
+# pip3 install TwitterSearch
+
+echo "Finished setup script."
