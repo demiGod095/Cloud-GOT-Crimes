8479d1a799fff945859e06587db4c9f521585d8e
ksundararaja <49825087+ksundararaja@users.noreply.github.com>
2019-05-13 04:00:16 +1000

    Update run-wp.sh

--- Ansible/Wordpress/run-wp.sh
+++ Ansible/Wordpress/run-wp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-. ./unimelb-comp90024-group-52-openrc.sh; ansible-playbook -i hosts -u ubuntu --key-file=~/.ssh/id_alwyn wordpress.yaml
+. ./unimelb-comp90024-group-52-openrc.sh; ansible-playbook -i hosts -u ec2-user --key-file=~/.ssh/sins101 wordpress.yaml
